dd7d645df60b437936d9f3b2673882c7ff04a7db
Hanie24 <sailor_121108@hotmail.com>
2018-04-04 08:58:08 -0500

    one

---
+++ app.js
@@ -0,0 +1,30 @@
+// let fecha = fecha => {
+//     let currentDate = Date.now();
+//     document.getElementById('fecha').innerHTML = currentDate;
+// }
+
+// let updateDate = date => {
+//     setInterval(function() {fecha();}, 1000);
+// }
+
+// updateDate();
+
+// class Ana {
+//     constructor(name, age) {
+//         this.name = 'ana luisa';
+//         this.age = 24;
+//     }
+//     yearsDog() {
+//     return this.age/7
+//     }
+//     capital() {
+//         return this.name.toUpperCase()
+//     }
+// }
+
+// let me = new Ana
+//     document.getElementById('my-name').innerText = me.capital()
+//     document.getElementById('my-age').innerText = me.yearsDog()
+
+class Mamifero {}
+
